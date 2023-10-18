912659ca2b3b0a665799b587db21fec5b8602ef5
IMXEren <96839938+IMXEren@users.noreply.github.com>
2023-10-18 21:34:48 +0530

    ⬆️ v1.7.3

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from setuptools import find_packages, setup
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-#wU2A6AagBtZSyEm
+
 setup(
     name='nextdnsapi',
     packages=find_packages(include=['nextdnsapi']),
-    version = '1.7.2',      # Start with a small number and increase it with every change you make
+    version = '1.7.3',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="I was getting increasingly frustrated with NextDNS's lack of API. I wanted to manage things on the fly. So, I did the most logical thing. I built a python script (library-to-be) to control my NextDNS account. I decided to make it public because why not?",
-    author_email = 'erpihmisr@relay.firefox.com',      # Type in your E-Mail
-    url = 'https://github.com/rhijjawi/NextDNS-API',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/rhijjawi/NextDNS-API/archivev1-7-0.tar.gz',    # I explain this later on
+    author_email = 'contact_NAPI@imx.anonaddy.io',      # Type in your E-Mail
+    url = 'https://github.com/IMXEren/nextdnsapi',   # Provide either the link to your github or to your website
     keywords = ['NEXTDNS', 'API', 'REQUESTS'],   # Keywords that define your package best
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[            # I get to this in a second
            'requests',
+           'pyotp'
        ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
@@ -25,8 +25,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     )
